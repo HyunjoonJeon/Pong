a9b5ea6387aedc8fb02ffca9f0e30ffa6ffbdd17
Ragviswa <rv519@ic.ac.uk>
2021-03-08 00:10:20 +0000

    FPGA to server with threadings Trial 2

--- local_processing/fpga_to_server_with_threading.py
+++ local_processing/fpga_to_server_with_threading.py
@@ -23,20 +23,25 @@ class ClientConsole():
         vals = output.stdout
         vals = vals.decode("utf-8")
         vals = vals.split('<-->')
-
         return vals[1].strip()
+
+    def UDPthread(self):
+        t1 = threading.Thread(target=self.UDPreceive)
+        t2 = threading.Thread(target=self.UDPsend)
+        t1.start()
+        t2.start()
+        
 
     def UDPsend(self):
         while True:
             self.sock.sendto(str.encode(self.send_on_jtag(self.command)), self.addressport)
-            t = threading.Thread(target=self.UDPreceive)
-            t.start()
 
     def UDPreceive(self):
-        msgfromserver = self.sock.recvfrom(self.buffersize)
-        msg = "Message from Server {}".format(msgfromserver[0])
-        print(msg)
-        self.command = msgfromserver.decode("utf-8")
+        while True:
+            msgfromserver = self.sock.recvfrom(self.buffersize)
+            msg = "Message from Server {}".format(msgfromserver[0])
+            print(msg)
+            self.command = msgfromserver.decode("utf-8")
     
 
     
