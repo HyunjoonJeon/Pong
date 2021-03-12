a5ecf1c96404867ba0b168d9b5da715a69e4ff14
HyunjoonJeon <hyunjoon.jeon19@imperial.ac.uk>
2021-03-12 13:54:35 +0100

    server with multiple socket

---
+++ server/server_multiple_socket.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+import time
+#import _thread
+
+class ServerConsole():
+
+    def __init__(self):
+        self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.localPort = 2399
+        self.bufferSize = 1024
+        self.sock.bind(("0.0.0.0", self.localPort))
+        self.currClientAddress = "empty"
+        self.threadCount = 0
+        self.clients = set() #stores addresses of the clients
+        #self.sockets = set() #stores sockets in case the server wants to access them
+        print("UDP Server up and listening")
+
+    def UDPthread(self, Client):
+        t1 = threading.Thread(target=self.UDPreceive, args=[Client])
+        t2 = threading.Thread(target=self.UDPsend, args=[Client])
+        t1.start()
+        t2.start()
+
+    def UDPreceive(self, Client):
+        while True:
+            data, addr = Client.recvfrom(self.bufferSize)
+            print("Message from Client:{}".format(data))
+            print("Client IP Address:{}".format(addr))
+    
+    def UDPsend(self, Client, msgFromServer="Test"):
+        while True:
+            #msgFromServer = input()
+            if self.currClientAddress != "empty":
+                bytesToSend = str.encode(msgFromServer)
+                Client.send(bytesToSend)
+                time.sleep(3)
+
+    def UDPserver(self):
+        while True:
+            message, address = self.sock.recvfrom(self.bufferSize)
+            self.clients.add(address)
+            if len(self.clients) > self.threadCount:
+                print("Connected to:{}".format(address))
+                newSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                newSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                self.localPort += 1 # new port
+                newSock.bind(("0.0.0.0",self.localPort))
+                self.sock.sendto(str.encode(str(self.localPort)), address)
+                #_thread.start_new_thread(self.UDPthread, (newSock, ))
+                primary = threading.Thread(target=self.UDPthread, args=[newSock])
+                primary.start()
+                self.threadCount += 1
+                print("Thread Number: " + str(self.threadCount))
+
+if __name__ == "__main__":
+    server = ServerConsole()
+    server.UDPserver()
