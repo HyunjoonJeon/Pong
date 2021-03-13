7844a1e8f57d3d8a41468740560340049858f2c5
Ragviswa <rv519@ic.ac.uk>
2021-03-13 09:43:18 +0000

    Threading killing test 1

--- server/server_multiple_socket.py
+++ server/server_multiple_socket.py
@@ -12,32 +12,47 @@ class ServerConsole():
         self.sock.bind(("0.0.0.0", self.localPort))
         self.currClientAddress = "empty"
         self.threadCount = 0
+        self.threadKill = None
         self.clients = set() #stores addresses of the clients
         #self.sockets = set() #stores sockets in case the server wants to access them
         print("UDP Server up and listening")
 
-    def UDPthread(self, Client, address):
+    def UDPthread(self, Client, address, threadcount):
         print("Thread " + str(self.threadCount) + " started")
-        t1 = threading.Thread(target=self.UDPreceive, args=[Client,address])
-        t2 = threading.Thread(target=self.UDPsend, args=[Client,address])
+        t1 = threading.Thread(target=self.UDPreceive, args=[Client,address, threadcount])
+        t2 = threading.Thread(target=self.UDPsend, args=[Client,address, threadcount])
         t1.start()
         t2.start()
 
-    def UDPreceive(self, Client, address):
+    def UDPreceive(self, Client, address, threadcount):
         while True:
-            data, addr = Client.recvfrom(self.bufferSize)
-            assert address == addr
-            self.currClientAddress = addr
-            print("Message from Client:{}".format(data))
-            print("Client IP Address:{}".format(addr))
+            rectime = time.time()
+            try:
+                data, addr = Client.recvfrom(self.bufferSize)
+            except socket.error:
+                if(time.time()>rectime+60):
+                    self.threadKill = Client
+                    print(f"Killed Receive Thread {threadcount}")
+                    return
+                else:
+                    continue
+            else:
+                assert address == addr
+                self.currClientAddress = addr
+                print("Message from Client:{}".format(data))
+                print("Client IP Address:{}".format(addr))
     
-    def UDPsend(self, Client, address, msgFromServer="Test"):
+    def UDPsend(self, Client, address, threadcount, msgFromServer="Test"):
         while True:
             #msgFromServer = input()
             #if self.currClientAddress != "empty":
             bytesToSend = str.encode(msgFromServer)
             Client.sendto(bytesToSend, address)
             time.sleep(3)
+            if self.threadKill == Client:
+                print(f"Killed Send Thread {threadcount}")
+                self.threadKill = None
+                return
 
     def UDPserver(self):
         while True:
@@ -51,7 +66,7 @@ class ServerConsole():
                 newSock.bind(("0.0.0.0",self.localPort))
                 self.sock.sendto(str.encode(str(self.localPort)), address)
                 #_thread.start_new_thread(self.UDPthread, (newSock, ))
-                primary = threading.Thread(target=self.UDPthread, args=[newSock, address])
+                primary = threading.Thread(target=self.UDPthread, args=[newSock, address, self.threadCount])
                 primary.start()
                 self.threadCount += 1
                 #print("Thread Number: " + str(self.threadCount))
